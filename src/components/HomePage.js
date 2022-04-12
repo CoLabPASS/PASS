039b349a816e333114744316e47fcf932374b0af
SaraMunir <sara.m07011@gmail.com>
2022-04-12 12:33:20 -0400

    added modal for sign up

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import NavBar from './NavBar'
 import arrowImg from '../Assets/next.png'
+import {Modal, Button} from 'react-bootstrap'
 
 function HomePage() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
         <NavBar/>
@@ -12,15 +18,41 @@ function HomePage() {
               <div className="headerDiv">
                 <h1>CTA Headline & <br/> Emotion Regulation</h1>
                 <div>
-                  <button>SignIn</button>
-                  <button>SignUp</button>
+                  <button >SignIn</button>
+                  <button onClick={handleShow}>SignUp</button>
+                  <Modal show={show} onHide={handleClose} animation={false} size="lg"
+                    aria-labelledby="contained-modal-title-vcenter"
+                    centered
+>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      <form>
+                        <label htmlFor="userName" className='sr-only'>User Name</label>
+                        <input type="text" placeholder='User Name' id="userName"/>
+                        <label htmlFor="userEmail" className='sr-only'>Email</label>
+                        <input type="email" placeholder='Email' id="userEmail" />
+                        <label htmlFor="password" className='sr-only'>password</label>
+                        <input type="password" placeholder='Password' id="password" />
+                        <label htmlFor="confirmPassword" className='sr-only'>Confirm Password</label>
+                        <input type="password" placeholder='confirmPassword' id="confirmPassword" />
+                      </form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
                 </div>
               </div>
             </div>
           </header> {/**** HEADER END ****/}
-
           <section className='appDescription'>
-
             <div className='appDescDiv wrapper'>
               <div className='appDescText'>
                 <h2>Line One <br/> Line 2</h2>
@@ -28,7 +60,6 @@ function HomePage() {
               </div>
               <div className='appDescImg'><img src="" alt="" /></div>
             </div>
-
             <div className='appDescDiv wrapper'>
               <div className='appDescImg'><img src="" alt="" /></div>
               <div className='appDescText'>
@@ -71,14 +102,11 @@ function HomePage() {
 
           </section> {/**** CALL TO ACTION SECTION END ****/}
 
-
-
           <footer>
             <div className="wrapper">
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi repudiandae, iure voluptates quis, ipsam necessitatibus dolorem sequi corrupti architecto, cum inventore aperiam distinctio omnis unde non temporibus nisi dolores fuga?</p>
             </div>
           </footer>
-
         </main>
     </>
   )
