7bc56f9a76b5fe8d19adddcb262150aae816a06e
SaraMunir <sara.m07011@gmail.com>
2022-05-07 15:19:29 -0400

    added drop down for menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link, useLocation, useNavigate } from "react-router-dom";
 
 
 function NavBar() {
     let navigate = useNavigate();
     let location = useLocation();
+    const [showOtherMenu, setShowOtherMenu]=useState(false)
     const localUserId = localStorage.userId;
     const signOut=()=>{
         localStorage.removeItem("userId");
@@ -46,30 +47,48 @@ function NavBar() {
                         <button className='navItem'>Sign Up</button>
                     </li>
                 }
-                {localUserId ?
-                    location.pathname === "/journals" ?null :
-
-                    <li>
-                        <Link to="/journals" className='navItem'>Journals</Link>
-                    </li> 
-                    :
-                    null
-                }
-                {localUserId ?
-                    location.pathname === "/Calendar" ?null :
-
-                    <li>
-                        <Link to="/Calendar" className='navItem'>Calendar</Link>
-                    </li> 
-                    :
-                    null
-                }
-                {localUserId ?
+                {/* {localUserId ?
                     <li>
                         <button className='navItem' onClick={signOut}>Sign Out</button>
                     </li> :
                     null
-                }
+                } */}
+                    <li >
+                        <button className='navItem otherBtn' onClick={()=>setShowOtherMenu(!showOtherMenu)}> 
+                        {/* <i class="fas fa-bars"></i> */}
+                            <i class="fas fa-caret-down"></i>
+                        </button>
+                        {showOtherMenu ?
+                            <ul className='otherMenu'>
+                                
+                                {localUserId ?
+                                    location.pathname === "/journals" ?null :
+
+                                    <li>
+                                        <Link to="/journals" className='navItem'><i class="fas fa-book"></i> &nbsp; Journals</Link>
+                                    </li> 
+                                    :
+                                    null
+                                }
+                                
+                                {localUserId ?
+                                    location.pathname === "/Calendar" ?null :
+
+                                    <li>
+                                        <Link to="/Calendar" className='navItem'><i class="fas fa-calendar-alt"></i> &nbsp; Calendar</Link>
+                                    </li> 
+                                    :
+                                    null
+                                }
+                                <li><button className='navItem' onClick={signOut}><i class="fas fa-sign-in-alt"></i> &nbsp;Sign Out</button></li>
+                            </ul>
+
+                        :
+                        null
+                        
+                        }
+
+                    </li> 
             </ul>
 
         </div>
